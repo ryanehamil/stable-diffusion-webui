28f9f4ed067110187ebe1e2a6a09a04ed2469a07
Ryan Hamilton <ryanehamil@outlook.com>
2023-03-29 19:49:34 -0500

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers
+set COMMANDLINE_ARGS=--listen --xformers --enable-insecure-extension-access --gradio-queue --share
 
 call webui.bat
