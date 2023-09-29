ddf2862636d1900242e466dc4238917b174154d6
Ryan Hamilton <ryanehamil@outlook.com>
2023-09-29 14:14:51 -0500

    use medvram

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --autolaunch --update-check --xformers --opt-sdp-attentio --enable-insecure-extension-access --update-check
+set COMMANDLINE_ARGS=--listen --autolaunch --update-check --opt-sdp-attentio --enable-insecure-extension-access --update-check --medvram --xformers
 
 call webui.bat
